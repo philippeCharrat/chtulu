ad1028bdafaa8f6359987331ccb0a2b5ee6db981
Philippe CHARRAT <charrat.p@gmail.com>
2021-02-23 07:43:43 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,20 @@
+function validerForm(){
+    var log = document.getElementById("log").innerHTML;
+    var rech_label = document.getElementById("recherche_label").innerHTML;
+    var rech_texte = document.getElementById("recherche_texte").value;
+    var new_log = log+"</br>"+rech_label+" "+rech_texte;
+    document.getElementById("recherche_texte").value = "";
+    document.getElementById("reponse").innerHTML = "";
+    
+    if (rech_texte.includes("search")) {
+      var user  = rech_texte.substr(7);
+      console.log("search "+user); 
+    }  else if (rech_texte.includes("help")) {
+      console.log("help");     
+      document.getElementById("reponse").innerHTML = "<ul><li> search user : Cherche un utilisateur </li><li> help : Affiche les commandes </li></ul>";
+    } else  {
+      console.log("nothing"); 
+    }
+    document.getElementById("log").innerHTML = new_log;
+    
+}
